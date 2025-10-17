567ff03405fa39dab19611f355cc15e8275fb40d
ansonsarturs <ansonsarturs@gmail.com>
2025-10-17 13:34:47 +0300

    Update serializers.py

--- shipping_service_app/shipping_service/apps/shipping/serializers.py
+++ shipping_service_app/shipping_service/apps/shipping/serializers.py
@@ -7,4 +7,4 @@ class ShipmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Shipment
         fields = ["id", "user_id", "order_id", "tracking_number", "status", "created_at", "updated_at"]
-        read_only_fields = ["id", "tracking_number", "status", "created_at", "updated_at"]
+        read_only_fields = ["id", "created_at", "updated_at"]
